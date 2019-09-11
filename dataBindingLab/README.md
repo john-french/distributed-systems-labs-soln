28351a41d080684e40755184fd652b2e7ca54585
John French <frenche@gmail.com>
2019-09-11 21:47:23 +0100

    Initial commit

---
+++ dataBindingLab/README.md
@@ -0,0 +1,14 @@
+# COMP08011 Distributed Systems
+## Data Binding: Text-Based Formats
+- Book example from https://www.vogella.com/tutorials/JAXB/article.html
+
+- Initial project with plain classes
+- Add jaxb api import
+- Add XML annotations
+- run example
+    - fails: no implementation
+- Add jaxb implementation
+
+- Repeat for json
+- Repeat for yaml
+- use xjc to create classes based on schema?
---
+++ dataBindingLab/src/ie/gmit/ds/BookMain.java
@@ -0,0 +1,87 @@
+package ie.gmit.ds;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import ie.gmit.ds.Book;
+import ie.gmit.ds.BookStore;
+
+public class BookMain {
+
+	private static final String BOOKSTORE_XML = "./bookstore-jaxb.xml";
+
+	public static void main(String[] args) throws JAXBException, IOException {
+
+		ArrayList<Book> bookList = new ArrayList<Book>();
+
+		// create books
+		Book book1 = new Book();
+		book1.setIsbn("978-0060554736");
+		book1.setName("The Game");
+		book1.setAuthor("Neil Strauss");
+		book1.setPublisher("Harpercollins");
+		bookList.add(book1);
+
+		Book book2 = new Book();
+		book2.setIsbn("978-3832180577");
+		book2.setName("Feuchtgebiete");
+		book2.setAuthor("Charlotte Roche");
+		book2.setPublisher("Dumont Buchverlag");
+		bookList.add(book2);
+
+		// create bookstore, assigning book
+		BookStore bookstore = new BookStore();
+		bookstore.setName("Fraport Bookstore");
+		bookstore.setLocation("Frankfurt Airport");
+		bookstore.setBookList(bookList);
+
+		// create JAXB context and instantiate marshaller
+		JAXBContext context = JAXBContext.newInstance(BookStore.class);
+		Marshaller jaxbMarshaller = context.createMarshaller();
+		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+		// Write to System.out
+		jaxbMarshaller.marshal(bookstore, System.out);
+
+		// Write to File
+		jaxbMarshaller.marshal(bookstore, new File(BOOKSTORE_XML));
+
+		// Object to JSON
+		ObjectMapper jsonMarshaller = new ObjectMapper();
+		String jsonBookStore = jsonMarshaller.writerWithDefaultPrettyPrinter().writeValueAsString(bookstore);
+		System.out.println(jsonBookStore);
+		FileWriter fileWriter = new FileWriter("./bookstore.json");
+		fileWriter.write(jsonBookStore);
+		fileWriter.close();
+
+		// YAML
+		ObjectMapper yamlMarshaller = new ObjectMapper(new YAMLFactory());
+		Book newBook = yamlMarshaller.readValue(new File("book1.yaml"), Book.class);
+
+		System.out.println(newBook.getName());
+		bookstore.addBookToList(newBook);
+		jaxbMarshaller.marshal(bookstore, new File(BOOKSTORE_XML));
+
+		// get variables from our xml file, created before
+		System.out.println();
+		System.out.println("Output from our XML File: ");
+		Unmarshaller um = context.createUnmarshaller();
+		BookStore bookstore2 = (BookStore) um.unmarshal(new FileReader(BOOKSTORE_XML));
+		ArrayList<Book> list = bookstore2.getBooksList();
+		for (Book book : list) {
+			System.out.println("Book: " + book.getName() + " from " + book.getAuthor());
+		}
+	}
+}
